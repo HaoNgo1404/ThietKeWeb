aa63f047ccfd6c262b9639bfdb297bdef0c1a88c
HaoNgo1404 <haodong14042005@gmail.com>
2024-10-10 11:40:29 +0700

    init

---
+++ ThietKeWeb/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThietKeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProDuctDetail()
+        {
+            return View();
+        }
+        public ActionResult Favourite()
+        {
+            return View();
+        }
+        public ActionResult Recruit()
+        {
+            return View();
+        }
+        public ActionResult Address()
+        {
+            return View();
+        }
+        public ActionResult GioHang()
+        {
+            return View();
+        }
+        public ActionResult ThanhToan()
+        {
+            return View();
+        }
+        public ActionResult CheckProduct()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        public ActionResult History()
+        {
+            return View();
+        }
+    }
+}
---
+++ ThietKeWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThietKeWeb.MvcApplication" Language="C#" %>
